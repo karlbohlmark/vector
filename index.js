5d7bf046aa9afc35d1d6c708c3179196c33e372d
Karl Böhlmark <karl.bohlmark@edgeware.tv>
2013-02-21 12:09:23 +0100

    use length of longest vector

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+var max = require('max');
+
 var sum = function () {
 	var args = [].slice.call(arguments);
-	var items = args[0].length;
+  // Use the length of the highest dimension vector
+  // Count undefined as 0
+	var items = max(args.map(function (v) { return v.length}));
 	var vectorSum = [];
 	for (var i = items; i; i--) {
 		vectorSum[i-1] = 0;
 		for (var v = args.length; v; v--) {
-			vectorSum[i-1] = vectorSum[i-1] + args[v-1][i-1];
+			vectorSum[i-1] = vectorSum[i-1] + args[v-1][i-1] || 0;
 		}
 	}
 	return vectorSum;
